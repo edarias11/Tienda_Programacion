5173a8a94fdfc92ebaa96b4f719a6a6f3a736dc3
edarias11 <elias11ariasr@gmail.com>
2025-12-17 22:36:55 -0600

    ultimo commit

--- src/main/java/com/tienda/MessageSourceConfig.java
+++ src/main/java/com/tienda/MessageSourceConfig.java
@@ -1,9 +1,18 @@
 package com.tienda;
 
+//Declara una configuración de Spring.
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+
+
+///Crea un bean MessageSource.
+///Carga el archivo messages.properties.
+
+//Permite usar mensajes internacionalizados (i18n) en la app.
+
+//Define que los mensajes se lean en UTF-8.
 
 @Configuration
 public class MessageSourceConfig {
@@ -17,3 +26,4 @@ public class MessageSourceConfig {
     }
    
 }
+
